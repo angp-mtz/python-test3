261f1d7a7529901d514c69c124a99aa28c67404b
angp-mtz <123033185+angp-mtz@users.noreply.github.com>
2023-10-10 14:01:36 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,14 @@
+splitString = "this string has ben \nsplit over n\several\nlines"
+# fixed the quotation mark
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+# fixed the quotation mark
+print(tabbedString)
+print('the pet shop owner said "no, no, \'he\'s uh, ... he\'s resting."')
+print("the pet shop owner said \"no, no, 'he's uh, ... he's resting.\"")
+print("""the pet shop owner said "no, no, 'he's uh, ... he's resting.\"""")
+anotherString = """this string has been
+split over
+several
+lines""" # fixed the quotation mark
+print(anotherString)
